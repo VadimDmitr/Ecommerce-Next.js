6d8dc058ff14c27c845e305a12164ebc29649f2e
VadimDmitr <vadzzimluch@gmail.com>
2023-07-02 11:24:08 -0400

    Add styles

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ export default async function Home() {
   console.log(products);
 
   return (
-    <main className="">
+    <main className="min-h-screen max-w-7xl mx-auto px-8">
       <section className="flex flex-col space-y-12 pb-44">
         <h1 className="text-5xl font-bold text-center">DEAL OF THE DAY</h1>
 
